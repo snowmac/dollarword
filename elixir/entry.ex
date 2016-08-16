47b1f5982ddfaf3bc6db6a64cf684b559a68313b
Adam Bourg <adam.bourg@gmail.com>
2016-08-16 08:41:52 -0600

    elixir commit

--- elixir/entry.ex
+++ elixir/entry.ex
@@ -1,6 +1,6 @@
 case File.read Path.expand("../words/Awords.csv") do
   {:ok, __} -> IO.puts "All is well you m fer"
-  {:error, error} -> IO.puts "The error is #{error} #{Path.expand("../words/A Words.csv")}"
+  {:error, error} -> IO.puts "The error is #{error} #{Path.expand("../words/Awords.csv")}"
 end
 
 # IO.puts file_handle
